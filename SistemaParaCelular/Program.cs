2f1b71506f7466f29c51816e570497586568f3ff
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2022-11-03 15:34:44 -0300

    Add files via upload

---
+++ SistemaParaCelular/Program.cs
@@ -0,0 +1,11 @@
+﻿using SistemaParaCelular.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "777111", modelo: "N01", imei: "121212", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "777222", modelo: "I12 PRO", imei: "121314", memoria: 138);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
